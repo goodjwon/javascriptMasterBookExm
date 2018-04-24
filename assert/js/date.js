df450e74693972123498b2dac029532017c838d0
goodjwon <goodjwon@gmail.com>
2018-04-25 07:59:19 +0900

    add date

---
+++ assert/js/date.js
@@ -0,0 +1,14 @@
+
+
+var d = new Date();
+console.log(d);
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getDate());
+var d = new Date('2018/12/04 20:07:15');
+console.log(d);
+var d = new Date(2016, 11, 4, 20, 07, 15, 500);
+console.log(d);
+var d = new Date(1480845765500);
+console.log(d);
+
