7c5fe0570c5d360c0a3e4652e0a88eefa3eb6708
goodjwon <jwon76@naver.com>
2018-04-30 22:29:03 +0900

    function add

--- assert/js/function.js
+++ assert/js/function.js
@@ -5,3 +5,16 @@ function getTriangle(base, height) {
 }
 
 console.log("삼각형의 면적은: "+getTriangle(5, 2));
+
+
+var getTriangle2 = function(base, height){
+	return base * height /2;
+};
+
+console.log("삼각형2의 면적은: "+getTriangle2(10,20));
+
+let getTriangle3 = (base, height) => {
+	return base * height /2;
+};
+
+console.log("삼각형3의 면적은: "+getTriangle3 (20, 50));
