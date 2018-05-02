0a74a08168927e2bfa58679a38025698ed13db8a
goodjwon <jwon76@naver.com>
2018-05-03 08:58:54 +0900

    function agrs added

--- assert/js/function.js
+++ assert/js/function.js
@@ -31,3 +31,31 @@ function printf(format){
 };
 
 printf('안녕하세요 {0}씨 나는 {1} 입니다.','시온', '피카추');
+
+
+
+
+
+
+
+function getTriangleArgs(args) {
+	if(args.base === undefined ) {args.base = 1;}	
+	if(args.height === undefined ) {height = 1;}
+	return args.base * args.height / 2
+};
+
+
+console.log("삼각형의 면적은 args : "+getTriangleArgs({base:5, height:10}));
+
+
+function getTriangelArgs2(base = 1, height =1) {
+	return base * height /2;
+}
+
+console.log("삼각형의 면적은 arg2 "+getTriangelArgs2(5));
+
+
+function multi(a = b, b = 5){
+	return a * b;
+}
+console.log(multi());
