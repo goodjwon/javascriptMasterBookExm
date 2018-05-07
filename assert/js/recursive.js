9f9e4363357d4303df4f9c7c709dab88f7609f19
goodjwon <jwon76@naver.com>
2018-05-08 07:58:50 +0900

    높은수준의 함수테마

--- assert/js/recursive.js
+++ assert/js/recursive.js
@@ -34,3 +34,26 @@ arrayWalk(ary, showElement);
 arrayWalk(ary, function (value, key) {
     console.log(key + ':' + value);
 });
+
+
+function escapeHtml(str){
+	if(!str) {return '';}
+	str = str.replace(/&/g, '&amp;');
+	str = str.replace(/</g, '&lt;');
+	str = str.replace(/>/g, '&gt;');
+	str = str.replace(/"/g, '&quot;');
+	str = str.replace(/'/g, '&#39;');
+
+	return str;
+}
+
+function e(templates, ...value){
+	let result = '';
+	for(let i = 0, len = templates.length; i < len; i++){
+		result += templates[i] + escapeHtml(value[i]);
+	}
+	return result;
+}
+
+let name = '<"Mario" &  \'Luigi \'>';
+console.log(e `안녕사세요, ${name}씨!` );
