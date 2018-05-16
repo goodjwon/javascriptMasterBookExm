b4a4469da721cb7b6b5f9cdf0687a9e783d59f23
goodjwon <jwon76@naver.com>
2018-05-17 08:00:30 +0900

    prototype chine

--- assert/js/chain.js
+++ assert/js/chain.js
@@ -1,22 +1,38 @@
-var Animal = function(){};
+var Animal = function(){};	//선언
 
 
-Animal.prototype = {
+Animal.prototype = {		//정의
 	walk : function(){
 		console.log('종종...');
 	}
 };
 
-var Dog = function(){
+
+var SuperAnimal = function(){};	//선언
+
+
+SuperAnimal.prototype = {		// walk 정의
+	walk : function(){
+		console.log('다다다닷!!');
+	}
+}
+
+
+var Dog = function(){			//선언 Animal 상속.
 	Animal.call(this);
 }
 
-Dog.prototype = new Animal();
-Dog.prototype.bark = function(){
-	console.log('멍멍!!');
-}
+Dog.prototype = new Animal();	//생성 
 
+ Dog.prototype.bark = function(){	//bark method 정의
+ 	console.log('멍멍!!');
+ }
 
-var d = new Dog();
+var d = new Dog();				
 d.walk();
-d.bark();
+//d.bark();
+
+Dog.prototype = new SuperAnimal();
+var d2 = new Dog();
+d2.walk();
+d.walk();
