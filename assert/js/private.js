be3fd8afe1942084e9a27abd4426d508a9ddc844
goodjwon <jwon76@naver.com>
2018-05-18 08:13:33 +0900

    add Rantalgle

--- assert/js/private.js
+++ assert/js/private.js
@@ -25,18 +25,49 @@ Traiagle.prototype.getArea = function(){
 	return this.getBase() * this.getHeight() / 2 ;
 }
 
+function Rantalgle(){
+	var _rbase;
+	var _rHeight;
+
+	this.setRbase = function(rbase){
+		_rbase = rbase;
+	}
+	this.getRbase = function(){
+		return _rbase;
+	}
+
+	this.setRhight = function(rHeight){
+		_rHeight = rHeight;
+	}
+	this.getRHight = function(){
+		return _rHeight = rHeight;
+	}
+}
+
+Rantalgle.prototype.getArea = function(){
+	return this._rbase * this._rHeight;
+}
 
 
-
+//member filed 입력했지만 값이 셋팅 되지 않는다.
 var t = new Traiagle();
 t._base =  10;
 t._height = 2;
-console.log(t.getArea());
+console.log(t.getArea()); //NaN
+console.log('')
 
-
+//set method에 했더니 값이 셋팅 된다.
 t.setBase(10);
 t.setHeight(2);
 
 console.log(t.getBase());
 console.log(t.getHeight());
 console.log(t.getArea());
+
+
+var r = new Rantalgle();
+r._rbase = 10;
+r._rHeight = 5;
+console.log(r._rbase);
+console.log(r.getRbase());
+console.log(r.getArea()); // 50 what is different between one on the top?
