8df14a4060be73eb05d21c00522cf23b7ff45974
goodjwon <jwon76@naver.com>
2018-05-23 14:33:02 +0900

    add getTeg, add selector

--- assert/js/element_id.js
+++ assert/js/element_id.js
@@ -5,3 +5,16 @@ var res = document.getElementById("result2");
 console.log(current.toLocaleString());
 
 res.textContent = current.toLocaleString();
+
+
+
+var list = document.getElementsByTagName('a');
+
+for(var i =0 , len = list.length; i < len ; i++){
+    console.log(list.item(i).href);
+}
+;
+
+
+var listSelector = document.querySelectorAll("#list a");
+console.log(listSelector.length);
