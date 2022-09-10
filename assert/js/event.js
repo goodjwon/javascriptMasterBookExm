4f6688a82281a5af68dbaa432267ca1da757e5ce
goodjwon <firstman9@protonmail.com>
2022-09-10 23:26:14 +0900

    이펜트 처리리 추가

--- assert/js/event.js
+++ assert/js/event.js
@@ -1,5 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('btn').addEventListener('click', function(){
+document.addEventListener('DOMContentLoaded', function(e){
+    console.log(e);
+    document.getElementById('btn').addEventListener('click', function(ea){
+        console.log(ea);
         window.alert('버튼이눌려졌다.');
     }, false);
 }, false);
+
+document.addEventListener('DOMContentLoaded', (event)=>{
+    console.log(event);
+    document.getElementById('btn').addEventListener('click', (ea)=>{
+        console.log(ea);
+        window.alert('버튼이눌려졌다.');
+    });
+})
